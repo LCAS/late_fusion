5d678ad09f0bc742971f3ea8d86fe4c926b5b3f0
ernstmv <ernestoroque777@gmail.com>
2025-04-30 10:29:42 +0100

    config.yaml and image_detector.launch.py added in image_detector_pkg

--- image_detector_pkg/launch/image_detector.launch.py
+++ image_detector_pkg/launch/image_detector.launch.py
@@ -4,18 +4,19 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python import get_package_share_directory
 
+
 def generate_launch_description():
     config_file = os.path.join(
-            get_package_share_directory("late_fusion_pkg"),
+            get_package_share_directory('image_detector_pkg'),
             'config',
             'config.yaml'
             )
 
-    return LaunchDescription([
+    return LaunchDescription(
             Node(
-                package='late_fusion_pkg',
-                executable='late_fusion_node',
-                name="late_fusion_node",
+                package='image_detector_pkg',
+                executable='image_detector_node',
+                name='image_detector_node',
                 parameters=[config_file]
                 )
-            ])
+            )
--- image_detector_pkg/setup.py
+++ image_detector_pkg/setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'image_detector_pkg'
@@ -5,14 +6,25 @@ package_name = 'image_detector_pkg'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(
+        exclude=[
+            'test'
+            ]
+        ),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/image_detector.launch.xml']),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'ultralytics',
+        'cv_bridge',
+        'numpy',
+        'torch',
+        ],
     zip_safe=True,
     maintainer='ernstmv',
     maintainer_email='ernestoroque777@gmail.com',
